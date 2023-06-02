62a4a6b5b0b6472dfa49c00bc297b322e3092786
Kirill <blzneci.89@gmail.com>
2023-06-02 18:12:38 +0300

    change example.py

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -1,2 +1,27 @@
+"""
+This is the "example" module.
+
+The example module supplies one function, reverse(). For example,
+
+>>> reverse('awesome!')
+'!emosewa'
+"""
+
+
 def reverse(string):
+    """Reverse string
+
+    >>> reverse('')
+    ''
+
+    >>> reverse('Hexlet')
+    'telxeH'
+    """
+
+
     return string[::-1]
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
